825d7cc98811e40407eaac85103d5f9b6642ee2f
Ollie Jones <oathompsonjones@gmail.com>
2024-08-03 16:29:22 +0100

    Equip in trinket slot by default instead of armour slot

--- src/main/java/uk/co/oathompsonjones/SocksItem.java
+++ src/main/java/uk/co/oathompsonjones/SocksItem.java
@@ -1,6 +1,7 @@
 package uk.co.oathompsonjones;
 
 import dev.emi.trinkets.api.Trinket;
+import dev.emi.trinkets.api.TrinketItem;
 import dev.emi.trinkets.api.TrinketsApi;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
@@ -19,4 +20,14 @@ public class SocksItem extends ArmorItem implements Trinket {
 
         TrinketsApi.registerTrinket(this, this);
     }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        // Attempts to equip the item as a trinket before falling back to the default behaviour
+        ItemStack itemStack = user.getStackInHand(hand);
+        boolean canEquipTrinket = TrinketItem.equipItem(user, itemStack);
+        return canEquipTrinket ? TypedActionResult.success(itemStack) : super.use(world, user, hand);
+
+    }
+
 }
