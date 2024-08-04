9e990733b2a813fef58c2e1ef417a196d2f38730
Ollie Jones <oathompsonjones@gmail.com>
2024-08-04 21:50:22 +0100

    Make the SocksArmourMaterial class a part of the SocksItem class

--- src/main/java/uk/co/oathompsonjones/SocksItem.java
+++ src/main/java/uk/co/oathompsonjones/SocksItem.java
@@ -2,8 +2,12 @@ package uk.co.oathompsonjones;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -29,5 +33,53 @@ public class SocksItem extends ArmorItem {
             return result.getResult().equals(ActionResult.SUCCESS) ? result : super.use(world, user, hand);
         }
         return super.use(world, user, hand);
+    }
+
+    private static class SocksArmourMaterial implements ArmorMaterial {
+        String name;
+
+        public SocksArmourMaterial(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public int getDurability(ArmorItem.Type type) {
+            return 0;
+        }
+
+        @Override
+        public int getProtection(ArmorItem.Type type) {
+            return 0;
+        }
+
+        @Override
+        public int getEnchantability() {
+            return 0;
+        }
+
+        @Override
+        public SoundEvent getEquipSound() {
+            return SoundEvents.ITEM_ARMOR_EQUIP_GENERIC;
+        }
+
+        @Override
+        public Ingredient getRepairIngredient() {
+            return null;
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public float getToughness() {
+            return 0;
+        }
+
+        @Override
+        public float getKnockbackResistance() {
+            return 0;
+        }
     }
 }
