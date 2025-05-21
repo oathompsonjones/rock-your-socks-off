888c7f5ea49b015e856eaccc97f8b8351dbc5751
Ollie Jones <oathompsonjones@gmail.com>
2025-05-21 21:29:36 +0100

    Fix Steel Skin effect

--- src/main/java/uk/co/oathompsonjones/mixin/StatusEffectMixin.java
+++ src/main/java/uk/co/oathompsonjones/mixin/StatusEffectMixin.java
@@ -2,6 +2,7 @@ package uk.co.oathompsonjones.mixin;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffects;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,7 +13,8 @@ import uk.co.oathompsonjones.RYSOStatusEffects;
 public class StatusEffectMixin {
     @Inject(method="applyUpdateEffect", at=@At("HEAD"), cancellable=true)
     private void ryso$applyUpdateEffect(LivingEntity entity, int amplifier, CallbackInfo ci) {
-        if (entity.hasStatusEffect(RYSOStatusEffects.STEEL_SKIN)) {
+        var effect = (StatusEffect) (Object) this;
+        if (entity.hasStatusEffect(RYSOStatusEffects.STEEL_SKIN) && effect == StatusEffects.POISON) {
             ci.cancel();
         }
     }
