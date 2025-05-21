1859007f5230bf9c8f5d81659ee445d0b04eb796
Ollie Jones <oathompsonjones@gmail.com>
2025-05-21 20:49:31 +0100

    Poisonous effect fully implemented

---
+++ src/main/java/uk/co/oathompsonjones/mixin/LivingEntityMixin.java
@@ -0,0 +1,39 @@
+package uk.co.oathompsonjones.mixin;
+
+import net.minecraft.entity.Attackable;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import uk.co.oathompsonjones.RYSOStatusEffects;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin extends Entity implements Attackable {
+    public LivingEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Shadow
+    public abstract boolean hasStatusEffect(StatusEffect effect);
+
+    @Inject(
+            method="damage", at=@At(
+            value="INVOKE",
+            target="Lnet/minecraft/entity/LivingEntity;setAttacker(Lnet/minecraft/entity/LivingEntity;)V",
+            shift=At.Shift.AFTER
+    )
+    )
+    public void ryso$damage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> ci) {
+        if (this.hasStatusEffect(RYSOStatusEffects.POISONOUS) && source.getAttacker() instanceof LivingEntity attacker)
+            attacker.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 120, 1), this);
+    }
+}
