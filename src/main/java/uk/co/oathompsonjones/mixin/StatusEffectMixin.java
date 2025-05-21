7ca0421be384dcdd9f3ca9920ba523bf39f5c214
Ollie Jones <oathompsonjones@gmail.com>
2025-05-21 21:21:55 +0100

    Steel skin effect fully implemented

---
+++ src/main/java/uk/co/oathompsonjones/mixin/StatusEffectMixin.java
@@ -0,0 +1,19 @@
+package uk.co.oathompsonjones.mixin;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import uk.co.oathompsonjones.RYSOStatusEffects;
+
+@Mixin(StatusEffect.class)
+public class StatusEffectMixin {
+    @Inject(method="applyUpdateEffect", at=@At("HEAD"), cancellable=true)
+    private void ryso$applyUpdateEffect(LivingEntity entity, int amplifier, CallbackInfo ci) {
+        if (entity.hasStatusEffect(RYSOStatusEffects.STEEL_SKIN)) {
+            ci.cancel();
+        }
+    }
+}
