7cf3d68467f10d6eb7a47aef9c076f0311b660b8
Ollie Jones <oathompsonjones@gmail.com>
2025-06-04 19:13:37 +0100

    Implement appropriate trades

--- src/main/java/uk/co/oathompsonjones/RYSOVillagers.java
+++ src/main/java/uk/co/oathompsonjones/RYSOVillagers.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
 import net.fabricmc.fabric.api.object.builder.v1.world.poi.PointOfInterestHelper;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
@@ -16,6 +17,9 @@ import net.minecraft.util.Identifier;
 import net.minecraft.village.TradeOffer;
 import net.minecraft.village.VillagerProfession;
 import net.minecraft.world.poi.PointOfInterestType;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class RYSOVillagers {
     public static final RegistryKey<PointOfInterestType> SOCKS_POI_KEY = poiKey("socks_poi");
@@ -45,13 +49,137 @@ public class RYSOVillagers {
     }
 
     public static void initialize() {
+        // Level 1: 6 emeralds for socks AND 4 emeralds for 16 cookies OR 4 wool for 1 emerald
         TradeOfferHelper.registerVillagerOffers(SOCKS_TRADER, 1, factories -> {
-            factories.add((entity, random) -> new TradeOffer(new ItemStack(Items.EMERALD, 32),
-                                                             new ItemStack(RYSOItems.SOCKS, 1),
+            factories.add((entity, random) -> new TradeOffer(new ItemStack(Items.EMERALD, 6),
+                                                             new ItemStack(RYSOItems.SOCKS),
+                                                             8,
+                                                             12,
+                                                             0.075f
+            ));
+        });
+        TradeOfferHelper.registerVillagerOffers(SOCKS_TRADER, 1, factories -> {
+            factories.add((entity, random) -> {
+                if (random.nextFloat() > 0.5)
+                    return new TradeOffer(new ItemStack(Items.EMERALD, 4),
+                                          new ItemStack(Items.COOKIE, 16),
+                                          8,
+                                          12,
+                                          0.075f
+                    );
+                var allWool = Arrays
+                        .stream(Items.class.getDeclaredFields())
+                        .filter(field -> field.getType()
+                                         == Item.class && field
+                                                 .getName()
+                                                 .endsWith("_WOOL"))
+                        .map(field -> {
+                            try {
+                                return (Item) field.get(null);
+                            } catch (IllegalAccessException e) {
+                                throw new RuntimeException("Failed to access item field: " + field.getName(), e);
+                            }
+                        })
+                        .toList();
+                return new TradeOffer(new ItemStack(allWool.get(random.nextInt(allWool.size())), 4),
+                                      new ItemStack(Items.EMERALD),
+                                      8,
+                                      12,
+                                      0.075f
+                );
+            });
+        });
+        // Level 2: 10 emeralds for a cake AND 16 dyes for 1 emerald
+        TradeOfferHelper.registerVillagerOffers(SOCKS_TRADER, 2, factories -> {
+            factories.add((entity, random) -> new TradeOffer(new ItemStack(Items.EMERALD, 10),
+                                                             new ItemStack(Items.CAKE),
                                                              6,
                                                              12,
                                                              0.075f
             ));
+        });
+        TradeOfferHelper.registerVillagerOffers(SOCKS_TRADER, 2, factories -> {
+            factories.add((entity, random) -> {
+                var allDyes = Arrays
+                        .stream(Items.class.getDeclaredFields())
+                        .filter(field -> field.getType()
+                                         == Item.class && field
+                                                 .getName()
+                                                 .endsWith("_DYE"))
+                        .map(field -> {
+                            try {
+                                return (Item) field.get(null);
+                            } catch (IllegalAccessException e) {
+                                throw new RuntimeException("Failed to access item field: " + field.getName(), e);
+                            }
+                        })
+                        .toList();
+                return new TradeOffer(new ItemStack(allDyes.get(random.nextInt(allDyes.size())), 16),
+                                      new ItemStack(Items.EMERALD),
+                                      6,
+                                      12,
+                                      0.075f
+                );
+            });
+        });
+        // Level 3: 40 emeralds and 10 bones for a vertebra OR 40 emeralds and 10 rotten flesh for a cursed flesh
+        TradeOfferHelper.registerVillagerOffers(SOCKS_TRADER, 3, factories -> {
+            factories.add((entity, random) -> {
+                if (random.nextFloat() > 0.5)
+                    return new TradeOffer(new ItemStack(Items.EMERALD, 40),
+                                          new ItemStack(Items.BONE, 10),
+                                          new ItemStack(RYSOItems.VERTEBRA),
+                                          4,
+                                          12,
+                                          0.075f
+                    );
+                return new TradeOffer(new ItemStack(Items.EMERALD, 40),
+                                      new ItemStack(Items.ROTTEN_FLESH, 10),
+                                      new ItemStack(RYSOItems.CURSED_FLESH),
+                                      4,
+                                      12,
+                                      0.075f
+                );
+            });
+        });
+        // Level 4: 32 emeralds and blank socks for Grandmas Socks
+        TradeOfferHelper.registerVillagerOffers(SOCKS_TRADER, 4, factories -> {
+            factories.add((entity, random) -> new TradeOffer(new ItemStack(Items.EMERALD, 32),
+                                                             new ItemStack(RYSOItems.SOCKS),
+                                                             // TODO: Change this to Grandma's Socks
+                                                             new ItemStack(RYSOItems.CHRISTMAS_SOCKS),
+                                                             4,
+                                                             12,
+                                                             0.075f
+            ));
+        });
+        // Level 5: 64 emeralds and blank socks for a pair of "coloured" socks
+        TradeOfferHelper.registerVillagerOffers(SOCKS_TRADER, 5, factories -> {
+            factories.add((entity, random) -> {
+                var colouredSocks = List.of(RYSOItems.LIGHT_GRAY_SOCKS,
+                                            RYSOItems.GRAY_SOCKS,
+                                            RYSOItems.BLACK_SOCKS,
+                                            RYSOItems.BROWN_SOCKS,
+                                            RYSOItems.RED_SOCKS,
+                                            RYSOItems.ORANGE_SOCKS,
+                                            RYSOItems.YELLOW_SOCKS,
+                                            RYSOItems.LIME_SOCKS,
+                                            RYSOItems.GREEN_SOCKS,
+                                            RYSOItems.CYAN_SOCKS,
+                                            RYSOItems.LIGHT_BLUE_SOCKS,
+                                            RYSOItems.BLUE_SOCKS,
+                                            RYSOItems.PURPLE_SOCKS,
+                                            RYSOItems.MAGENTA_SOCKS,
+                                            RYSOItems.PINK_SOCKS
+                );
+                return new TradeOffer(new ItemStack(Items.EMERALD, 64),
+                                      new ItemStack(RYSOItems.SOCKS),
+                                      new ItemStack(colouredSocks.get(random.nextInt(colouredSocks.size()))),
+                                      6,
+                                      12,
+                                      0.075f
+                );
+            });
         });
     }
 }
